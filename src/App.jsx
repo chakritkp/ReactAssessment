05fc03faaa5d5c14346891c1ae9382f28b7c3bb5
Chakrit <chakritkp@hotmail.com>
2023-09-13 16:32:35 +0700

    delete Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { useEffect, useState } from 'react'
-import axios from "axios";
-
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './App.css'
 import HomeNormal from './HomeNormal'
-import HomeUser from './HomeUser';
-import HomeAdmin from './HomeAdmin'
-import Owner from './Owner'
-import Layout from './Layout'
-import Login from './login';
+import './App.css'
 
 const App = () => {
 
